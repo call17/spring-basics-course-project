e9b988a02958c7069be35b494a59f54e3cd1071c
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-17 09:59:05 +0400

    fix test

--- src/test/java/com/yet/spring/core/TestApp.java
+++ src/test/java/com/yet/spring/core/TestApp.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 import com.yet.spring.core.beans.EventType;
 import org.junit.Test;
@@ -25,27 +24,24 @@ public class TestApp {
     @Test
     public void test() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         Client client = new Client("25", "Bob");
-        Map<EventType, EventLogger> stub = new HashMap();
-        stub.put(EventType.ERROR,new DummyLogger());
+        DummyLogger dummyLogger = new DummyLogger();
 
-        App app = new App(client,new DummyLogger(), stub);
-        
+        App app = new App(client, dummyLogger, new HashMap<>());
+
         Event event = new Event(new Date(), DateFormat.getDateTimeInstance());
-        
-        invokeLogEvent(app, event, MSG + " " + client.getId());
-//        assertTrue(dummyLogger.getEvent().getMsg().contains(MSG));
-//        assertTrue(dummyLogger.getEvent().getMsg().contains(client.getFullName()));
-//
-//        invokeLogEvent(app, event, MSG + " 0");
-//        assertTrue(dummyLogger.getEvent().getMsg().contains(MSG));
-//        assertFalse(dummyLogger.getEvent().getMsg().contains(client.getFullName()));
+
+        invokeLogEvent(app, event, EventType.INFO);
+        assertTrue(dummyLogger.getEvent().getMsg().contains(client.getFullName()));
+
+        invokeLogEvent(app, event, EventType.INFO);
+        assertFalse(dummyLogger.getEvent().getMsg().contains(client.getFullName()));
     }
 
-    private void invokeLogEvent(App app, Event event, String message) throws NoSuchMethodException,
-            IllegalAccessException, InvocationTargetException {
-        Method method = app.getClass().getDeclaredMethod("logEvent", Event.class, String.class);
+    private void invokeLogEvent(App app, Event event, EventType eventType) throws NoSuchMethodException,
+        IllegalAccessException, InvocationTargetException {
+        Method method = app.getClass().getDeclaredMethod("logEvent", EventType.class, Event.class);
         method.setAccessible(true);
-        method.invoke(app, event, message);
+        method.invoke(app, eventType, event);
     }
     
     private class DummyLogger implements EventLogger {
